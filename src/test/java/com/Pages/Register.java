4f5e98ed4a7943fa8f4e5bd1fe33122fbf234e43
ThiagoMaurat <thiagomaurat@hotmail.com>
2022-12-15 22:42:47 -0300

    fix:register/login

--- src/test/java/com/Pages/Register.java
+++ src/test/java/com/Pages/Register.java
@@ -12,6 +12,11 @@ public class Register extends BasePage {
     public static final String confirmPassword = "/html/body/div/div/div[2]/div/div[2]/form/div[5]/div/input";
     public static final String confirmRegister = "/html/body/div/div/div[2]/div/div[2]/form/button";
     public static final String successMessage = "/html/body/div/div/div[3]/div/div[2]/p";
+    public static String closeModal = "/html/body/div/div/div[3]/div/div[2]/a";
+    public static String emailInput = "/html/body/div/div/div[2]/div/div[1]/form/div[1]/input";
+    public static String passwordInput = "/html/body/div/div/div[2]/div/div[1]/form/div[2]/div/input";
+    public static String acessButton = "/html/body/div/div/div[2]/div/div[1]/form/div[3]/button[1]";
+    public static String verifyLabel = "/html/body/div/div/div[3]/div[1]/p";
 
     public void getInRegisterPag() throws InterruptedException {
         WebElement myAccountButton = getWebElement(By.xpath(registerButton));
@@ -43,5 +48,26 @@ public class Register extends BasePage {
         System.out.println(verifyMessageSucess.getText());
         return verifyMessageSucess.getText();
 
+    }
+
+    public void closeModalButton() throws InterruptedException {
+        WebElement closeWebElement = getWebElement(By.xpath(closeModal));
+        closeWebElement.click();
+        Thread.sleep(2000);
+    }
+
+    public void FillInput() throws InterruptedException {
+        insertIntoField(emailInput, "test-0a37vixu5@srv1.mail-tester.com");
+        insertIntoField(passwordInput, "Thiago123");
+    }
+
+    public void AcessButton() {
+        WebElement buttonPage = getWebElement(By.xpath(acessButton));
+        buttonPage.click();
+    }
+
+    public String verifyIfTextSucessed() {
+        WebElement textAsert = getWebElement(By.xpath(verifyLabel));
+        return textAsert.getText();
     }
 }
--- src/test/java/com/Tests/RegisterLoginTest.java
+++ src/test/java/com/Tests/RegisterLoginTest.java
@@ -1,15 +1,12 @@
 package com.Tests;
 
 import static org.junit.jupiter.api.Assertions.*;
-
-import com.Pages.Login;
 import com.Pages.Register;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class RegisterTest {
+class RegisterLoginTest {
 	private static Register page;
-	private static Login page2;
 
 	@BeforeEach
 	public void setUp() {
@@ -24,14 +21,11 @@ class RegisterTest {
 		page.confirmRegister();
 		assertTrue(
 				page.verifyIfSucessed().contains("foi criada com sucesso"));
-	}
-
-	@Test
-	public void searchIphoneTestAndAddToCart() throws InterruptedException {
-		page2.FillInput();
-		page2.AcessButton();
+		page.closeModalButton();
+		page.FillInput();
+		page.AcessButton();
 		Thread.sleep(2000);
-		assertTrue(page2.verifyIfTextSucessed().contains("Saldo em conta"));
+		assertTrue(page.verifyIfTextSucessed().contains("Saldo em conta"));
 	}
 
 }
